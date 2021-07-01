1f0a46e51d845911da6bf95130f72adf68b1e02b
503203440 <503203440@qq.com>
2021-07-02 00:33:39 +0800

    增加了系统托盘和单个app运行锁

---
+++ src/main/java/io/yx/asktask/SingleAppLock.java
@@ -0,0 +1,45 @@
+package io.yx.asktask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+/**
+ * 避免多次运行同一个java程序
+ */
+public class SingleAppLock {
+
+    private static final Logger log = LoggerFactory.getLogger(SingleAppLock.class);
+
+    static FileLock fileLock = null;
+    static FileChannel fileChannel = null;
+    static RandomAccessFile raf = null;
+
+    public static boolean lock(String key) {
+
+        File file = new File(System.getProperty("java.io.tmpdir") + key + ".lock");
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            // 尝试获取文件锁
+            raf = new RandomAccessFile(file, "rw");
+            fileChannel = raf.getChannel();
+            fileLock = fileChannel.tryLock();
+            if (fileLock != null) {
+                log.info("获取锁成功");
+                return true;
+            }
+        } catch (Exception e) {
+            log.error("ERROR", e);
+        }
+
+        return false;
+    }
+
+}

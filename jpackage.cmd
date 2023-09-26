bb35de37e539627c8a819d45be023a764325d2b7
YX <503203440@qq.com>
2023-09-26 15:08:44 +0800

    编译打包输出结果中包含公司信息,不使用zgc,因为在windows低于1803的版本中无法使用ZGC

--- jpackage.cmd
+++ jpackage.cmd
@@ -30,5 +30,5 @@ REM for /f %%i in ('call jdeps --print-module-deps --ignore-missing-deps %~dp0ta
 
 set modules=java.base,java.compiler,java.desktop,java.management,java.naming,java.rmi,java.scripting,java.sql,jdk.httpserver,java.sql,java.security.sasl,java.security.jgss,java.xml.crypto,jdk.charsets,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.crypto.mscapi
 echo %modules%
-
-jpackage.exe --type app-image -i "%sourceFolder%" -n asktask --java-options "-Djava.net.preferIPv4Stack=true -XX:+UseZGC -Xmx100m" --main-jar %MAIN_JAR_NAME% --icon %iconFilePath% --add-modules %modules% -d "%~dp0%target"
+@REM VirtualAlloc2 Error occurred during initialization of VM ZGC requires Windows version 1803 or later
+jpackage.exe --type app-image -i "%sourceFolder%" -n asktask --java-options "-Djava.net.preferIPv4Stack=true -Xmx100m" --main-jar %MAIN_JAR_NAME% --icon %iconFilePath% --add-modules %modules% -d "%~dp0%target" --copyright "gpai.net" --description "Regular rotation training interface, recording abnormal response logs" --vendor "gpai.net"
